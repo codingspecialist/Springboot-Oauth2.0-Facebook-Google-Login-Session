8910dfeb602c61059fb0f5ec1fd7c7ead7fb5203
ssar <ssar@nate.com>
2019-12-23 21:19:57 +0900

    Springboot-Oauth2.0-Social-Login-UsingSession

---
+++ src/main/java/com/example/springsocial/controller/AuthController.java
@@ -0,0 +1,68 @@
+package com.example.springsocial.controller;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.springsocial.exception.BadRequestException;
+import com.example.springsocial.model.AuthProvider;
+import com.example.springsocial.model.Role;
+import com.example.springsocial.model.User;
+import com.example.springsocial.payload.ApiResponse;
+import com.example.springsocial.payload.SignUpRequest;
+import com.example.springsocial.repository.UserRepository;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // mustache templates
+    @GetMapping("/signin")
+    public String login() {
+    	return "loginForm";
+    }
+    
+    // postman
+    @PostMapping("/signup")
+    public @ResponseBody ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new BadRequestException("Email address already in use.");
+        }
+
+        // Creating user's account
+        User user = new User();
+        user.setName(signUpRequest.getName());
+        user.setEmail(signUpRequest.getEmail());
+        user.setPassword(signUpRequest.getPassword());
+        user.setProvider(AuthProvider.local);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.valueOf("USER"));
+        
+        User result = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/user/me")
+                .buildAndExpand(result.getId()).toUri();
+
+        return ResponseEntity.created(location)
+                .body(new ApiResponse(true, "User registered successfully@"));
+    }
+
+}
